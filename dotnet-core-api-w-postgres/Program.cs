86573033392a57c6bb7e9fd1c0d2110646122b1f
gooddogdevelopment-sys <gooddogdevelopment@gmail.com>
2026-03-20 13:56:40 -0400

    Swagger, ping controller, serilog, exception handler, dbcontext

--- dotnet-core-api-w-postgres/Program.cs
+++ dotnet-core-api-w-postgres/Program.cs
@@ -1,10 +1,41 @@
+using dotnet_core_api_w_postgres.Data;
+using dotnet_core_api_w_postgres.Middleware;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .Enrich.FromLogContext()
+    .CreateLogger();
+builder.Host.UseSerilog();
+
+// Database
+builder.Services.AddDbContext<AppDbContext>(opts =>
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+// Exception handling (RFC 9457 Problem Details)
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Template", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Bearer {token}\""
+    });
+});
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
@@ -12,9 +43,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseSerilogRequestLogging(); //Optional. Logs all Http requests
 
 app.UseAuthorization();
 
